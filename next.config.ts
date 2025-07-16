1c529a786d5b5984f89cfae268081a955dc16c1a
obeyhades <abdu.nazar145@gmail.com>
2025-07-17 00:39:28 +0200

    feat: add for imgs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
+import { dataset, projectId } from "@/sanity/env";
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        pathname: `/images/${projectId}/${dataset}/**`,
+      },
+    ],
+  },
 };
 
 export default nextConfig;
