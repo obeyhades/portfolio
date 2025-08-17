e7c0cf61a8037bd48a955fde2760529c0baa0c62
obeyhades <abdu.nazar145@gmail.com>
2025-08-17 18:00:01 +0200

    fix: solved error

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { client } from "@/sanity/lib/client";
 import { projectQuery } from "@/sanity/lib/queries";
 import { Project } from "@/sanity/types/project";
