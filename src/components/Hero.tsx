1079083f0230d52573d38cf99235a7cdd68534e6
obeyhades <abdu.nazar145@gmail.com>
2026-03-25 17:00:32 +0100

    style: add animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,6 +2,8 @@
 
 import { Homepage } from "@/sanity/types/homepage";
 import Navbar from "./Navbar";
+import { ChevronDown } from "lucide-react";
+import { motion } from "motion/react";
 
 type HeroProps = {
   homepage: Homepage;
@@ -10,7 +12,7 @@ type HeroProps = {
 export default function Hero({ homepage }: HeroProps) {
   return (
     <section
-      id="hero" 
+      id="hero"
       className="relative w-full h-screen flex items-center justify-center text-center"
       style={{
         backgroundImage: `url(${homepage.heroImage})`,
@@ -29,7 +31,32 @@ export default function Hero({ homepage }: HeroProps) {
         <p className="mt-4 text-lg md:text-2xl max-w-2xl mx-auto">
           {homepage.description}
         </p>
+        <motion.div
+          className="mt-10 flex justify-center"
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.4 }}
+        >
+          <a href="#contact">
+            <button
+              className="px-6 py-3 border border-zinc-300/80 rounded-lg text-white uppercase tracking-widest text-sm md:text-base hover:bg-white hover:text-black transition">
+              Contact me
+            </button>
+          </a>
+        </motion.div>
       </div>
+
+      <motion.div
+        className="absolute bottom-8 text-white"
+        animate={{ y: [0, 10, 0] }}
+        transition={{ duration: 1.5, repeat: Infinity, ease: "easeInOut" }}
+      >
+        <ChevronDown
+          size={48}
+          strokeWidth={1.5}
+          className="mx-auto opacity-80"
+        />
+      </motion.div>
     </section>
   );
 }
